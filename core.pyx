273d1db1dcd444ae59a89e465ee53c28bae83f97
Benjamin Smith <minimiffy@btinternet.com>
2024-04-09 00:52:40 +0100

    adding tax calculations, including taxfree allowance calculations - pending including pension contributions

--- core.pyx
+++ core.pyx
@@ -1,10 +1,54 @@
 import cython
 
 
-cdef class FinanceCore:
+cdef class FinanceCore(object):
+    cdef float tax_free_allowance, lower_tax_bracket, middle_tax_bracket, upper_tax_bracket
 
     def __init__(self) -> None:
-        pass
+        self.tax_free_allowance = 1048
+        self.lower_tax_bracket = 4189
+        self.middle_tax_bracket = 8333.33
+        self.upper_tax_bracket = 10428.33
 
-    def calculate_interest_from_rate(self, prelim_value: float, rate:float):
-        return prelim_value * rate
+    cdef calculate_interest_on_value_from_rate(self, value: float, rate:float):
+        """
+        Calculates interest on value from rate
+        :param value: float, value you want the interest to be calculated on
+        :param rate: float, the prevailing interest rate.
+        :return: The interest to be added to the value
+        """
+        return value * rate
+
+    cdef calculate_tax_from_monthly_income(self, monthly_income: float):
+        """
+        Calculates tax from monthly income
+        :param monthly_income: float, the monthly income
+        :return: The tax to be deducted from the monthly income
+        """
+        cdef float tax = 0.0
+        if monthly_income <= self.tax_free_allowance:
+            return tax
+        elif monthly_income <= self.lower_tax_bracket:
+            tax += (monthly_income-self.tax_free_allowance) * 0.2
+        elif monthly_income <= self.middle_tax_bracket:
+            tax += (self.lower_tax_bracket-self.tax_free_allowance) * 0.2
+            tax += (monthly_income -self.lower_tax_bracket)* 0.4
+        else:
+            tax += (self.lower_tax_bracket - self.tax_free_allowance) * 0.2
+            tax += (monthly_income - self.lower_tax_bracket) * 0.4
+            tax += monthly_income * 0.45
+        return
+
+    cdef calculate_new_tax_free_allowance(self, monthly_income: float):
+        """
+        Calculates the new tax free allowance
+        :param monthly_income: float, the monthly income
+        :return: The new tax free allowance
+        """
+        cdef float new_allowance
+        if monthly_income >= self.upper_tax_bracket:
+            new_allowance = 0
+        elif monthly_income >= self.middle_tax_bracket:
+            new_allowance = self.tax_free_allowance - (((monthly_income * 12) - (self.middle_tax_bracket*12)) *0.5)
+        return new_allowance
+
